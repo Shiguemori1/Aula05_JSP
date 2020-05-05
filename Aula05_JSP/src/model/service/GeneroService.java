16c68194d4c0dd3c2bfbb3b337dcbfc96b1dbd4c
Shiguemori1 <murilosensei@hotmail.com>
2020-05-05 20:08:48 -0300

    Aula 05 feita Criado alteração de filme Criado exclusão de filme Cirado listar todos os filmes

---
+++ Aula05_JSP/src/model/service/GeneroService.java
@@ -0,0 +1,19 @@
+package model.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import model.dao.GeneroDAO;
+import model.entity.Genero;
+
+public class GeneroService {
+	public ArrayList<Genero> listarGeneros() throws IOException {
+		GeneroDAO dao = new GeneroDAO();
+		return dao.listarGeneros();
+	}
+	
+	public Genero buscarGenero(int id) throws IOException {
+		GeneroDAO dao = new GeneroDAO();
+		return dao.buscarGenero(id);
+	}
+}
